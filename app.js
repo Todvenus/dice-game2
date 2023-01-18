64db71281d943aa7e359cec0167f573562c9a1ff
Tod <todvenus@gmail.com>
2023-01-18 13:59:58 +0800

    shoog shiddeg bolgow

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ var score = [0, 0];
 //Eeljiin onoo xuwisagch
 var roundScore = 0;
 // Shonii ali talaataa buusang xadgalax xuwisagch  
-var dice = Math.floor (Math.random()*6) + 1;
+var diceNumber = Math.floor (Math.random()*6) + 1;
 
-console.log("shoo :" +dice);
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var diceNumber = Math.floor (Math.random()*6) + 1;
+    diceDom.style.display = "block";
+diceDom.src = 'dice-' + diceNumber + '.png';
+});
+
+
+
+console.log("shoo : " + diceNumber);
 
